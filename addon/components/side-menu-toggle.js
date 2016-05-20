7b9f4c4c98c5d2c95873d6acc1c453c0fcc83c72
Tomasz Subik <tsubik@gmail.com>
2016-05-20 19:10:10 +0200

    side menu toggle styles

--- addon/components/side-menu-toggle.js
+++ addon/components/side-menu-toggle.js
@@ -3,7 +3,6 @@ import Ember from "ember";
 export default Ember.Component.extend({
     sideMenu: Ember.inject.service(),
 
-    tagName: "button",
     classNames: ["side-menu-toggle"],
 
     click() {
