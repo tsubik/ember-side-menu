3028045ae0db157f747e1af9cf649039364c7c88
Tomasz Subik <tsubik@gmail.com>
2016-09-12 11:36:12 +0200

    adding tests for ember 2.4 lts - ember try

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,36 +1,47 @@
 /*jshint node:true*/
 module.exports = {
-  scenarios: [
-    {
-      name: 'default',
-      dependencies: { }
-    },
-    {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
-      name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
-      }
-    }
-  ]
+    scenarios: [
+        {
+            name: "default",
+            dependencies: { }
+        },
+        {
+            name: "ember-2-4-lts",
+            bower: {
+                dependencies: {
+                    "ember": "~2.4.0"
+                },
+                resolutions: {
+                    "ember": "~2.4.0"
+                }
+            }
+        },
+        {
+            name: "ember-release",
+            dependencies: {
+                "ember": "components/ember#release"
+            },
+            resolutions: {
+                "ember": "release"
+            }
+        },
+        {
+            name: "ember-beta",
+            dependencies: {
+                "ember": "components/ember#beta"
+            },
+            resolutions: {
+                "ember": "beta"
+            }
+        },
+        {
+            name: "ember-canary",
+            dependencies: {
+                "ember": "components/ember#canary"
+            },
+            resolutions: {
+                "ember": "canary"
+            }
+        }
+    ]
 };
