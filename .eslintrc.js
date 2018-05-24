b595bbab02b91013236cc24684130c5a77c956c6
Tomasz Subik <tsubik@gmail.com>
2018-05-24 11:53:59 +0200

    upgrade node, change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
@@ -13,39 +12,9 @@ module.exports = {
   ],
   env: {
     "browser": true,
-    "es6": true,
     "node": true
   },
   rules: {
 
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        'index.js',
-        'testem.js',
-        'ember-cli-build.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js'
-      ],
-      excludedFiles: [
-        'app/**',
-        'addon/**',
-        'tests/dummy/app/**'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
-    }
-  ]
+  }
 };
