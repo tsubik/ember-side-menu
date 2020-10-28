f2d66e7d209367306d1525c81e870d246cc1dac1
Tomasz <tsubik@gmail.com>
2020-10-28 14:18:12 +0100

    compile css, auto import complied css if less or sass is not used

---
+++ compile-css.js
@@ -0,0 +1,14 @@
+const sass = require('node-sass');
+const fs = require('fs');
+const path = require('path');
+
+const inputFile = path.join(__dirname, 'app', 'styles', 'ember-side-menu.scss');
+const outputFile = path.join(__dirname, 'vendor', 'ember-side-menu.css');
+const buf = fs.readFileSync(inputFile, 'utf8');
+
+// Compile main file
+const result = sass.renderSync({
+  data: buf
+});
+
+fs.writeFileSync(outputFile, result.css);
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-side-menu'
+  name: 'ember-side-menu',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    const hasSass = !!app.registry.availablePlugins['ember-cli-sass'];
+    const hasLess = !!app.registry.availablePlugins['ember-cli-less'];
+
+    if (!hasSass && !hasLess) {
+      app.import('vendor/ember-side-menu.css');
+    }
+  }
 };
